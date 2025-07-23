b170f8e26c0389d85e23ef81e5ae2d9e6da1dbac
Karl Kula <store.karl.kula@gmail.com>
2025-07-23 14:13:59 +0200

    Create file for common things between player types

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
-all: kort.o player.o
+all: kort.o player.o player_common.o
 	gcc $^ 
 
-ai: kort.o computer_player.o
+ai: kort.o computer_player.o player_common.o
 	gcc $^ 
---
+++ player_common.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "kort.h"
+
+/* utilities */
+int calc_hand_size(struct state state);
+
+void identify_king_ace_transfer(struct state state);
+	
+/* cosmetics */
+void print_aces(struct state state);
+
+void print_kings(struct state state);
+
+void print_piles(struct state state);
+
+void print_hand_not_reordered(struct state state, int *order, int order_count);
+
+void print_hand(struct state state);
+
+void print_state(struct state state);
+
+/* automation */
+void calc_new_hand_order(struct state state, int *new_hand_order);
+	
+bool try_play_pile(struct state state, struct player_action *pa);
+
+bool try_play_hand(struct state state, struct player_action *pa);
+
+
